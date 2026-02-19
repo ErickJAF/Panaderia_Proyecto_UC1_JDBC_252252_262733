b091b2fde5158593faea80b1ca29b47cf9c79e63
isa1ascm <isaias.coronado252252@potros.itson.edu.mx>
2026-02-18 23:54:08 -0700

    contrasenia
    cambie la contraseña para que me jalara en mi lap

--- src/main/java/persistencia/conexion/ConexionBD.java
+++ src/main/java/persistencia/conexion/ConexionBD.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 public class ConexionBD implements IConexionBD {
     private final String CADENA_CONEXION = "jdbc:mysql://localhost:3306/sistema_panaderia";
     private final String USUARIO = "root";
-    private final String CONTRASENIA = "itson";
+    private final String CONTRASENIA = "Holapinto1";
 
     @Override
     public Connection crearConexion() throws SQLException {
